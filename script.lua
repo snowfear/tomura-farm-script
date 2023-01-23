0b7c74abaedf9e9730b6fb8180d7ce51a843d47f
snowfear <70731690+snowfear@users.noreply.github.com>
2023-01-23 11:06:55 -0600

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,16 @@
 -- Made by snow_fear#7003
+
+if loaded == true then
+
+	game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title = "Loaded",
+		Text = "The script is already running!",  -- Checks if script is already loaded
+		Icon = "rbxassetid://6022668888"
+	})
+	return
+end
+
+pcall(function() getgenv().loaded = true end)
 -- Instances:
 
 local Main = Instance.new("ScreenGui")
@@ -9,69 +21,73 @@ local UICorner = Instance.new("UICorner")
 
 Main.Name = "Main"
 Main.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Main.ResetOnSpawn = false
 
 Button.Name = "Button"
 Button.Parent = Main
 Button.BackgroundColor3 = Color3.fromRGB(0, 4, 255)
 Button.BorderSizePixel = 0
-Button.Position = UDim2.new(0.705338001, 0, 0.27510047, 0)
-Button.Size = UDim2.new(0, 108, 0, 31)
+Button.Position = UDim2.new(0.760210335, 0, 0.27510047, 0)
+Button.Size = UDim2.new(0, 50, 0, 31)
 Button.Font = Enum.Font.LuckiestGuy
-Button.Text = "Undefined"
+Button.Text = "Start"
 Button.TextColor3 = Color3.fromRGB(255, 255, 255)
-Button.TextScaled = true
-Button.TextSize = 14.000
+Button.TextSize = 17.000
 Button.TextWrapped = true
 
 UICorner.Parent = Button
 
 -- Scripts:
 
-local function DAFFX_fake_script() -- Button.Toggle 
+local function YTNSK_fake_script() -- Button.Toggle 
 	local script = Instance.new('LocalScript', Button)
 
-	local Encendido = true
 	local Player = game:GetService("Players").LocalPlayer
 	local valid = true
-	local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-	local humanoid = game.Players.LocalPlayer.Character.Humanoid
-	local location = CFrame.new(-47.752182, 1896.27734, 206.116669)
-	
-	function Imprimir(State)
+	local args = {
+		[1] = CFrame.new(1,1,1) -- Any args work here lmao
+	}
+
+	function repeating(State)
 		if State == "On" then
-			Player.PlayerGui.Main.Button.Text = "Encendido"
-			Encendido = false
+			Player.PlayerGui.Main.Button.Text = "On"
 			valid = false
 			State = "Off"
+			game.Players.LocalPlayer.CharacterAdded:Connect(function()
+				valid = true
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")  -- Debug
+				valid = false
+			end)
 			while valid == false do
-				wait(0.5)
-				game:GetService("Players").LocalPlayer.Character.Clones.Z:FireServer()
-				pl.CFrame = location
-			end
-		else
-			print(State)
-			Player.PlayerGui.Main.Button.Text = "Apagado"
-			Encendido = true
+				wait(0.1)
+				game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+                game:GetService("Players").LocalPlayer.Character:WaitForChild("Main") -- The function itself (Kinda messy)
+                game:GetService("Players").LocalPlayer.Character.Main:WaitForChild("Swing")
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-47.752182, 1896.27734, 206.116669) -- Tomura's CFrame
+				game:GetService("Players").LocalPlayer.Character.Main.Swing:FireServer(unpack(args))
+		end 
+	else
+			Player.PlayerGui.Main.Button.Text = "Off"
 			State = "On"
 			valid = true
-			end
 		end
-	
-	Player.PlayerGui.Main.Button.MouseButton1Click:Connect(function()
-		if Encendido then
-			Imprimir("On")
+	end
+
+	Player.PlayerGui.Main.Button.MouseButton1Click:Connect(function() -- A little bit more clean
+		if valid then
+			repeating("On")
 		else
-			Imprimir("Off")
-			end
+			repeating("Off")
+		end
 	end)
-	
+
 end
-coroutine.wrap(DAFFX_fake_script)()
-local function BZHTEMD_fake_script() -- Button.Drag 
+coroutine.wrap(YTNSK_fake_script)()
+local function DICPVYF_fake_script() -- Button.Drag 
 	local script = Instance.new('LocalScript', Button)
 
-	--Credits go to whoever made this script.
-	
+	--Credits go to whoever made this script. (This one runs independently, it just makes the button to drag smoothly, but i could certainly be replace by the legacy draggable)
+
 	local UIS = game:GetService("UserInputService")
 	function drag (Frame)
 		dragToggle = nil
@@ -108,6 +124,6 @@ local function BZHTEMD_fake_script() -- Button.Drag
 		end)
 	end
 	drag(script.Parent)
-	
+
 end
-coroutine.wrap(BZHTEMD_fake_script)()
+coroutine.wrap(DICPVYF_fake_script)()
